fec7f82a791026b05c09653e81a3f6f1d1746385
Roger Selwyn <rogers@selwyn.org.uk>
2021-09-13 14:21:47 +0100

    Latest updates

--- shell_scripts/run_dropbox_sync.sh
+++ shell_scripts/run_dropbox_sync.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-mkdir /mnt/data
-mount /dev/sda1 /mnt/data
-
 # Set Variables
+echo Set Variables
 DROPBOX_UPLOADER=/config/shell_scripts/dropbox_uploader.sh
 BKP_CONFIG=/config/shell_scripts/.dropbox_uploader
-BKP_DIR=/mnt/data/usr/share/hassio/backup
+BKP_DIR=/backup
 BKP_FOLDER=/debian
 
 # Copy Home Assistant Snapshots to DropBox
+echo Sync Dropbox
 $DROPBOX_UPLOADER -sf $BKP_CONFIG upload "$BKP_DIR" $BKP_FOLDER
 
 # Clean up older snapshots - removes older than 5 days
+echo Clean up old snapshots
 find "$BKP_DIR"/* -mtime +4 -exec rm {} \;
-umount /mnt/data
-rmdir /mnt/data
+
+# Reload Snapshots in Home Assistant
+echo Reload snapshots
+ha sn reload
+
+# mkdir /mnt/data
+# mount /dev/sda1 /mnt/data
+# umount /mnt/data
+# rmdir /mnt/data
