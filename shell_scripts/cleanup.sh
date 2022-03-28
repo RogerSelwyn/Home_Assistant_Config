8c71c6030b2a26a71cf1c9433428ad0145acf827
Roger Selwyn <rogers@selwyn.org.uk>
2022-03-28 16:13:37 +0100

    Latest updates

---
+++ shell_scripts/cleanup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function tidy_up () {
+    dirName=$1
+    days=$2
+
+    echo Clean up images older than $days days in $dirName
+    calcdays=$((days-1))
+    find "$dirName"/* -mtime +$calcdays -exec rm {} \;
+}
+
+# Set Variables
+echo Set Variables
+NEST_DIR_1=/config/nest/event_media/0ff91e4893c087da1e5fbdcef44684f1
+NEST_DIR_2=/config/nest/event_media/da5a578f24992239d2359242d1d8d600
+RING_DIR_1=/media/day
+RING_DIR_2=/media/night
+
+# Clean up old images
+tidy_up $NEST_DIR_1 7
+tidy_up $NEST_DIR_2 7
+tidy_up $RING_DIR_1 2
+tidy_up $RING_DIR_2 2
