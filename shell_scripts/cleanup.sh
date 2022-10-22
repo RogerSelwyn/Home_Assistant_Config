8cbeb36e5302ee112a50f3783006f9fb855d5188
Roger Selwyn <rogers@selwyn.org.uk>
2022-10-22 21:16:03 +0100

    Latest updates

--- shell_scripts/cleanup.sh
+++ shell_scripts/cleanup.sh
@@ -15,9 +15,12 @@ NEST_DIR_1=/config/nest/event_media/0ff91e4893c087da1e5fbdcef44684f1
 NEST_DIR_2=/config/nest/event_media/da5a578f24992239d2359242d1d8d600
 RING_DIR_1=/media/day
 RING_DIR_2=/media/night
+RING_DIR_3=/media/ding
+
 
 # Clean up old images
 tidy_up $NEST_DIR_1 7
 tidy_up $NEST_DIR_2 7
 tidy_up $RING_DIR_1 2
 tidy_up $RING_DIR_2 2
+tidy_up $RING_DIR_3 2
